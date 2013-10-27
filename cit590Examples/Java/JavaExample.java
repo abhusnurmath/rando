6e0944655704c124216678d22e8a401e2ba88b2d
arv <bhusnur4@seas.upenn.edu>
2013-10-27 05:32:36 -0400

    date program

--- cit590Examples/Java/JavaExample.java
+++ cit590Examples/Java/JavaExample.java
@@ -3,10 +3,24 @@ package cit590;
 public class JavaExample {
 
 	public static void main(String[] args) {
-		int n = 1;
-		while (n < 5){
-			System.out.println(n + " square is " + n * n);
-			n++;
+		int start = 1990;
+		int end = 2015;
+		int year = start;
+		boolean isLeapYear;
+
+		while (year <= end) {
+			isLeapYear = year % 4 == 0; // a leap year is a year divisible by
+										// 4...
+			if (isLeapYear && year % 100 == 0) { // ...but not by 100...
+				if (year % 400 == 0)
+					isLeapYear = true; // ...unless it’s also divisible by 400
+				else
+					isLeapYear = false;
+			}
+			if (isLeapYear) {
+				System.out.println(year + " is a leap year.");
+			}
+			year = year + 1;
 		}
 	}
 
