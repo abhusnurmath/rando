45e48359f95c9f8768809f22a57396c7bf3eb0b3
arv <bhusnur4@seas.upenn.edu>
2013-10-26 20:29:54 -0400

    adding a temporary example

---
+++ cit590Examples/Java/JavaExample.java
@@ -0,0 +1,13 @@
+package cit590;
+
+public class JavaExample {
+
+	public static void main(String[] args) {
+		int n = 1;
+		while (n < 5){
+			System.out.println(n + " square is " + n * n);
+			n++;
+		}
+	}
+
+}
