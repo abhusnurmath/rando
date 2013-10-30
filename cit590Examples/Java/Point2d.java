06c8d7d02ca66f92f87816764ad17d494852b90c
arv <bhusnur4@seas.upenn.edu>
2013-10-30 15:25:27 -0400

    a really simple stub for a 2d point that the class will fill iin

---
+++ cit590Examples/Java/Point2d.java
@@ -0,0 +1,15 @@
+package cit590;
+
+public class Point2d {
+
+	//what defines a 2d point?
+	
+	public Point2d() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	//what methods should we try and write?
+	//do points get added and subtracted? 
+	//what are valid transformations of points?
+
+}
