c468baa76126d975f137206e64326571cd6f52df
arv <bhusnur4@seas.upenn.edu>
2013-11-04 10:04:01 -0500

    added a new constructor

--- cit590Examples/Java/Rational.java
+++ cit590Examples/Java/Rational.java
@@ -11,31 +11,59 @@ public class Rational {
 		this.numerator = numerator;
 		this.denominator = denominator;
 	}
-	
-	public String toString(){
-		if (this.getNumerator() == 0){
+
+	/**
+	 * Given a "p/q" string make the rational number
+	 * 
+	 * @param s
+	 */
+	public Rational(String pSlashQ) {
+		// see if there is a slash
+		if (pSlashQ.contains("/")) {
+			String[] s = pSlashQ.split("/");
+			int numerator = Integer.parseInt(s[0]);
+			int denominator = Integer.parseInt(s[1]);
+			this.numerator = numerator;
+			this.denominator = denominator;
+		}
+		else{
+			// see if this is a number
+			try{
+				int num = Integer.parseInt(pSlashQ);
+				this.numerator = num;
+				this.denominator = 1;
+			}
+			catch (NumberFormatException e){
+				System.out.println("random bad input. try again");
+			}
+		}
+	}
+
+	public String toString() {
+		if (this.getNumerator() == 0) {
 			return "0";
 		}
-		if (this.getDenominator() == 1){
+		if (this.getDenominator() == 1) {
 			return "" + this.getNumerator();
 		}
 		return this.getNumerator() + "/" + this.getDenominator();
 	}
-	
-	public Rational reduceToLowestTerms(){
-		if (numerator == 0){
+
+	public Rational reduceToLowestTerms() {
+		if (numerator == 0) {
 			this.numerator = 0;
 			this.denominator = 1;
 		}
-		//the highest possible common factor is the min of the numerator and the denominator
+		// the highest possible common factor is the min of the numerator and
+		// the denominator
 		int maxPossibleFactor = Math.min(numerator, denominator);
-		for (int i=2; i<=maxPossibleFactor; i++){
-			while ((numerator%i == 0)&&(denominator%i ==0)){
-				this.numerator = this.numerator/i;
-				this.denominator = this.denominator/i;
+		for (int i = 2; i <= maxPossibleFactor; i++) {
+			while ((numerator % i == 0) && (denominator % i == 0)) {
+				this.numerator = this.numerator / i;
+				this.denominator = this.denominator / i;
 			}
 		}
-		if ((this.numerator < 0) && (this.denominator < 0)){
+		if ((this.numerator < 0) && (this.denominator < 0)) {
 			this.numerator = this.numerator * (-1);
 			this.denominator = this.denominator * (-1);
 		}
@@ -43,33 +71,34 @@ public class Rational {
 	}
 
 	public Rational add(Rational f) {
-		//actual solution begins here
+		// actual solution begins here
 		int denominator = f.denominator * this.denominator;
-		int numerator = f.numerator * this.denominator + this.numerator * f.denominator;
+		int numerator = f.numerator * this.denominator + this.numerator
+				* f.denominator;
 		return new Rational(numerator, denominator).reduceToLowestTerms();
 	}
-	
+
 	public Rational multiply(Rational f) {
-		//actual solution begins here
+		// actual solution begins here
 		int denominator = f.denominator * this.denominator;
 		int numerator = f.numerator * this.numerator;
 		return new Rational(numerator, denominator).reduceToLowestTerms();
 	}
-	
-	public boolean equals(Object arg0){
+
+	public boolean equals(Object arg0) {
 		Rational r = (Rational) arg0;
-		return (this.numerator == r.numerator) && (this.denominator == r.denominator);
+		return (this.numerator == r.numerator)
+				&& (this.denominator == r.denominator);
 	}
-	
-	
-	public double getDecimal(){
-		return (1.0*numerator)/denominator;
+
+	public double getDecimal() {
+		return (1.0 * numerator) / denominator;
 	}
-	
+
 	public int getNumerator() {
 		return numerator;
 	}
-	
+
 	public void setNumerator(int numerator) {
 		this.numerator = numerator;
 	}
@@ -81,5 +110,12 @@ public class Rational {
 	public void setDenominator(int denominator) {
 		this.denominator = denominator;
 	}
+	
+	public static void main(String[] args) {
+		Rational r = new Rational("1/3");
+		System.out.println(" NUM " + r.getNumerator());
+		System.out.println(" den " + r.getDenominator());
+		Rational r1 = new Rational("2");
+		System.out.println("den " + r1.getDenominator());
+	}
 }
-
