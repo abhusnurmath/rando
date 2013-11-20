179e1a93df77cb09d7603f841f66c3977dd57b1c
arv <bhusnur4@seas.upenn.edu>
2013-11-20 13:17:41 -0500

    a simple example of increment and decrement working harmoniously post synchronization

--- cit590Examples/Java/threads/SimpleDecrementer.java
+++ cit590Examples/Java/threads/SimpleDecrementer.java
@@ -2,30 +2,30 @@ package threads;
 
 import java.lang.Thread;
 
-public class SimpleIncrementer implements Runnable{
+public class SimpleDecrementer implements Runnable {
 
 	Student s;
 
-	public SimpleIncrementer(Student s){
+	public SimpleDecrementer(Student s) {
 		this.s = s;
 	}
 
 	@Override
 	public void run() {
-		for (int i = 0; i <10; i ++){
+		for (int i = 0; i < 10; i++) {
 			try {
 				Thread.sleep((int) (Math.random() * 1000));
 			} catch (InterruptedException e) {
 			}
-			// synchronized(s){
-			s.score = s.score + 1;
+			// synchronized (s) {
+			s.score = s.score - 1;
 			
 			try {
 				Thread.sleep((int) (Math.random() * 100));
-			} catch (InterruptedException e) {
+			} catch (Exception e) {
 				System.out.println("thread interrupted!");
 			}
-			System.out.println("i - hey the student's score is " + s.score);
+			System.out.println("d - hey the student's score is " + s.score);
 			// }
 		}
 	}
