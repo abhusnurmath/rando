f894eb075c95b4743289616fe200853fd9712e4a
arv <bhusnur4@seas.upenn.edu>
2013-12-01 15:25:52 -0500

    handle the exception

--- cit590Examples/Java/Files/src/files/ReadFile.java
+++ cit590Examples/Java/Files/src/files/ReadFile.java
@@ -1,11 +1,17 @@
 package files;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 
 public class ReadFile {
 
 	public static void main(String[] args) {
-		BufferedReader inputStream = new BufferedReader(new FileReader("xanadu.txt"));
+		try {
+			BufferedReader inputStream = new BufferedReader(new FileReader("xanadu.txt"));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
