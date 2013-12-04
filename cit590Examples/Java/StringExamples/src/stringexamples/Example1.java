cbd426d0a0ee6b885f0c84b3eb63e8245e070368
arv <bhusnur4@seas.upenn.edu>
2013-12-04 05:23:54 -0500

    a substring example

---
+++ cit590Examples/Java/StringExamples/src/stringexamples/Example1.java
@@ -0,0 +1,20 @@
+package stringexamples;
+
+public class Example1 {
+	public static void main(String[] args) {
+		String s1 = "Computer Science";
+		int x = 590;
+		String s2 = s1 + " " + x;
+		String s3 = s2.substring(10,17);
+		String s4 = "is fun";
+		String s5 = s2 + s4;
+		
+		System.out.println("s1: " + s1);
+		System.out.println("s2: " + s2);
+		System.out.println("s3: " + s3);
+		System.out.println("s4: " + s4);
+		System.out.println("s5: " + s5);
+		
+	
+	}
+}
