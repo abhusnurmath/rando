ae3f51f7f5c8898c37ee28e6a8fa79ceb6867772
arv <bhusnur4@seas.upenn.edu>
2014-01-13 13:06:46 -0500

    a while loop example

---
+++ cit590Examples/Python/simpleStuff/multiplicationTable.py
@@ -0,0 +1,10 @@
+# a simple program for multiplication tables
+x = input("Please enter a number between 2 and 20 that you would like the multiplication tables for")
+multiplier = 1;
+if 2<=x<=20:
+    while multiplier <= 20:
+        print x," times ", multiplier, " is ", x*multiplier
+        multiplier = multiplier + 1
+else:
+    print "Invalid input"
+    
