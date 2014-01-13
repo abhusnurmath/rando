f4605db8203a76d5037e9c087cc55333656b7838
arv <bhusnur4@seas.upenn.edu>
2014-01-13 11:52:36 -0500

    some very simple examples

---
+++ cit590Examples/Python/simpleStuff/simpleMath.py
@@ -0,0 +1,17 @@
+# a simple set of experiments to check out what each operator does
+myFavInt = 2
+
+print myFavInt ** 3 
+
+print myFavInt * 3
+
+print myFavInt <> myFavInt
+
+print 47 % (myFavInt + 1)
+
+print 'a' + 'b' #string concat
+
+print 345.6//20 #divide and floor
+
+
+
---
+++ cit590Examples/Python/simpleStuff/simpleStrings.py
@@ -0,0 +1,20 @@
+# a simple print statement
+print "Always look on the bright side of life"
+#single quotes work as well
+print 'Hello world'
+
+#variables
+v = "Arvind"
+
+#concatenation in a print statement using a variable
+print "Always look on the bright side of life" , "," , v
+
+#multi line strings - use triple quotes
+var = """The long and winding road that leads
+to your door, will never disappear, I've seen that road
+before"""
+
+print var
+#just hit F5 to run this
+
+
