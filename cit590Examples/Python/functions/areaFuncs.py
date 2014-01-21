c3a668a0ff608c7b89d199aad24239bdfa40f42b
arv <bhusnur4@seas.upenn.edu>
2014-01-21 18:06:27 -0500

    a simple example of an import

---
+++ cit590Examples/Python/functions/areaFuncs.py
@@ -0,0 +1,13 @@
+import math
+
+def areaOfTriangle(base, height):
+    area = 0.5 * base * height
+    return area
+
+def areaofCircle(radius):
+    area = math.pi * radius ** 2
+    return area
+
+def areaOfSquare(length):
+    area = length * length
+    return area
---
+++ cit590Examples/Python/functions/usageOfFuncs.py
@@ -0,0 +1,5 @@
+import areaFuncs
+
+height = 10
+base = 5
+print 'The area of the triangle is ', areaFuncs.areaOfTriangle(base, height)
