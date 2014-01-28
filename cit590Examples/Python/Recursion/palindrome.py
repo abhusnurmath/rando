e475ea336052fb0869ef95c18fc2ce16c306c5a3
arv <bhusnur4@seas.upenn.edu>
2014-01-27 20:32:34 -0500

    fixed bug

--- cit590Examples/Python/Recursion/palindrome.py
+++ cit590Examples/Python/Recursion/palindrome.py
@@ -7,6 +7,6 @@ def palTest(s):
     if not isLetter(s[-1]): return palTest(s[:-1])
     # a string is a palindrome if the last 2 letters of the string are the same,
     # and the remaining string also satisfies the condition of being a palindrome
-    elif s[0].lower != s[-1].lower: return False
+    elif s[0].lower() != s[-1].lower(): return False
     else: return palTest(s[1:len(s)-1])
 
