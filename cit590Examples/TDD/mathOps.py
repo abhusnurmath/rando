bba5061557638aeb2f362ed78e064f2e7155db02
arv <bhusnur4@seas.upenn.edu>
2014-02-03 12:19:35 -0500

    more commits in extremely raw form. recommit whatever we do in class

---
+++ cit590Examples/TDD/mathOps.py
@@ -0,0 +1,5 @@
+def squareRoot(x):
+    return 456
+
+def solveQuadratic(coeffList):
+    return 'whatever'
---
+++ cit590Examples/TDD/testMath.py
@@ -0,0 +1,13 @@
+from mathOps import *   # import everything from your module
+import unittest  # This loads the testing methods and a main program
+
+class TestMath(unittest.TestCase):  # use any meaningful name
+
+    ## Your test methods go here. Indent your
+    ## methods, because they belong inside the class.
+    def testSquareRoot(self):
+       
+
+unittest.main()  # outside the class--this tells the framework to run
+
+    
