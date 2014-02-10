562aa2a3c239b275755e02a20341a3eb81b1a086
arv <bhusnur4@seas.upenn.edu>
2014-02-09 21:41:58 -0500

    mergesort being added as an example

---
+++ cit590Examples/Python/Recursion/mergeSort.py
@@ -0,0 +1,32 @@
+def mergeSort(a):
+    if len(a)==0 or len(a) == 1:
+        return a
+    else:
+        firstHalf = mergeSort(a[:len(a)/2])
+        secondHalf = mergeSort(a[len(a)/2:])
+        return merge(firstHalf, secondHalf)
+
+def merge(a1, a2):
+    a3 = []
+    len1 = len(a1)
+    len2 = len(a2)
+    i=j=0
+    while i < len1 and j < len2:
+        if a1[i] < a2[j]:
+            a3.append(a1[i])
+            i += 1
+        else:
+            a3.append(a2[j])
+            j += 1
+    # we would either have gone through
+    # all of a1 or all of a2 by this time
+    # just tack on the remainging stuff at the end
+    if i < len1:
+        a3.extend(a1[i:])
+    if j < len2:
+        a3.extend(a2[j:])
+    return a3    
+            
+
+                
+
