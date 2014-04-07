6fb70bd62d8ffcf79e3bdcff3aa74bd83dee21e1
abhusnurmath <bhusnur@gmail.com>
2014-04-07 04:38:01 -0400

    make the grid layout example extend jframe instead

--- cit590Examples/Java/GUI/GridLayoutExample.java
+++ cit590Examples/Java/GUI/GridLayoutExample.java
@@ -1,19 +1,36 @@
 package guiprograms;
 
+import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.Toolkit;
 
-import javax.swing.JApplet;
 import javax.swing.JButton;
+import javax.swing.JFrame;
 
-public class GridLayoutExample extends JApplet {
+public class GridLayoutExample extends JFrame {
 
-	public void init () {
-		setLayout(new GridLayout (2,4));        
+	public void createGUI () {
+		setLayout(new GridLayout (2,2));        
 		add(new JButton("One"));
 		add(new JButton("Two"));  
 		add(new JButton("Three"));
 		add(new JButton("Four"));
 		add(new JButton("Five"));         
-		add(new JButton("Six"));      
+		add(new JButton("Six"));
+		render();
 	}
+	
+	private void render(){
+	    pack();
+        setSize(640, 480);
+        //place in the center
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        this.setLocation(dim.width/2-this.getSize().width/2, dim.height/2-this.getSize().height/2);
+        //remember to do this!
+        setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+        new GridLayoutExample().createGUI();
+    }
 }
