98d36d49b70362274c97754a44b3accb6320c763
abhusnurmath <bhusnur@gmail.com>
2014-04-07 05:02:18 -0400

    modified the degree slider example

--- cit590Examples/Java/GUI/DegreeSlider.java
+++ cit590Examples/Java/GUI/DegreeSlider.java
@@ -1,18 +1,25 @@
 package guiprograms;
 
+import java.awt.Dimension;
+
 import javax.swing.JSlider;
 import javax.swing.SwingConstants;
 
 public class DegreeSlider extends JSlider {
 
 	public DegreeSlider() {
-		this.setMinimum(0);
-		this.setMaximum(10);
+		this.setMinimum(-250);
+		this.setMaximum(250);
+		this.setPreferredSize(new Dimension(100,600));
 		this.setOrientation(SwingConstants.VERTICAL);
 		this.setPaintTicks(true);
 		this.setPaintLabels(true);
-		this.setMajorTickSpacing(2);
-		this.setMinorTickSpacing(1);
+		this.setMajorTickSpacing(10);
+		this.setMinorTickSpacing(5);
+	}
+	
+	public static void main(String[] args) {
+		//TODO implement this
 	}
 
 }
--- cit590Examples/Java/GUI/SliderExample.java
+++ cit590Examples/Java/GUI/SliderExample.java
@@ -1,32 +1,48 @@
 package guiprograms;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class SliderExample extends JFrame {
 	
-	DegreeSlider ds;
+	DegreeSlider cs;
+	DegreeSlider fs;
 	JButton submit;
 
 	public SliderExample() {}
 	
 	public void createGUI() {
-		this.setLayout(new FlowLayout());
-		ds = new DegreeSlider();
-		this.add(ds);
+	    JPanel p1 = new JPanel();
+		p1.setLayout(new FlowLayout());
+		JLabel celsius = new JLabel("celsius");
+		p1.add(celsius);
+		cs = new DegreeSlider();
+		p1.add(cs);
+		JLabel fahrenheit = new JLabel("fahrenheit");
+		p1.add(fahrenheit);
+		fs = new DegreeSlider();
+		p1.add(fs);
+		this.add(p1,BorderLayout.NORTH);
 		submit = new JButton("submit");
-		this.add(submit);
 		submit.addActionListener(new ClickListener());
+		this.add(submit,BorderLayout.CENTER);
 		showIt();
 	}
 	
 	public void onSubmit(){
-		System.out.println(ds.getValue());
+		int celsius = cs.getValue();
+		int fahr = (int)(9/5.0 * celsius + 32.0);
+		fs.setValue(fahr);
 	}
 	
 	public static void main(String[] args) {
@@ -41,14 +57,37 @@ public class SliderExample extends JFrame {
 	private void showIt(){
 		this.setMinimumSize(new Dimension(640,480));
 		this.setLocation(50, 100);
+		pack();
 		this.setVisible(true);
 	}
 
-	class ClickListener implements ActionListener{
+	class ClickListener implements KeyListener, ActionListener{
 
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
 			onSubmit();
+		}
+
+		@Override
+		public void keyPressed(KeyEvent arg0) {
+			// TODO Auto-generated method stub
+			// TODO Auto-generated method stub
+			// TODO Auto-generated method stub
+			// TODO Auto-generated method stub
+			// TODO Auto-generated method stub
+			// TODO Auto-generated method stub
+		}
+
+		@Override
+		public void keyReleased(KeyEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void keyTyped(KeyEvent arg0) {
+			// TODO Auto-generated method stub
+			
 		}
 		
 	}
