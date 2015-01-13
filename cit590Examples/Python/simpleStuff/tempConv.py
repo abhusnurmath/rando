e1b8623253b3f9493a125d273ccd468fbc9a4a6f
abhusnurmath <bhusnur@gmail.com>
2015-01-13 16:01:04 -0500

    temp conversion added

---
+++ cit590Examples/Python/simpleStuff/tempConv.py
@@ -0,0 +1,9 @@
+#simple temperature converter
+print "This program will convert a temperature in Celsius"
+print "to the equivalent temperature in Fahrenheit and Kelvin"
+c = input("What is temp in Celsius?")
+f = c * 9/5 + 32
+print "The equivalent temp in Fahrenheit is", f
+k = c + 273
+print "The equivalent temperature in Kelvin is", k
+
