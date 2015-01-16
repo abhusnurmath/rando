e93b866dec57bf656078f63412dc154f043e2faa
abhusnurmath <bhusnur@gmail.com>
2015-01-16 09:22:30 -0500

    showing multiple line input

--- cit590Examples/Python/simpleStuff/multiplicationTable.py
+++ cit590Examples/Python/simpleStuff/multiplicationTable.py
@@ -1,10 +1,11 @@
+
 # a simple program for multiplication tables
-x = input("Please enter a number between 2 and 20 that you would like the multiplication tables for \n")
+x = input('Please enter a number between 2 and 20'
+          ' that you would like the multiplication tables for \n')
 multiplier = 1;
 if 2 <= x <= 20:
     while multiplier <= 20:
-        print x," times ", multiplier, " is ", x*multiplier
+        print x," times ", multiplier, " is ", x * multiplier
         multiplier = multiplier + 1
 else:
     print "Invalid input"
-    
