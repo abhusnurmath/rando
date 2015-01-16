1d7352f1ac62dca91a10e07566da2426b818f455
abhusnurmath <bhusnur@gmail.com>
2015-01-16 09:14:07 -0500

    slight formatting change to multiplication tables

--- cit590Examples/Python/simpleStuff/multiplicationTable.py
+++ cit590Examples/Python/simpleStuff/multiplicationTable.py
@@ -1,7 +1,7 @@
 # a simple program for multiplication tables
-x = input("Please enter a number between 2 and 20 that you would like the multiplication tables for")
+x = input("Please enter a number between 2 and 20 that you would like the multiplication tables for \n")
 multiplier = 1;
-if 2<=x<=20:
+if 2 <= x <= 20:
     while multiplier <= 20:
         print x," times ", multiplier, " is ", x*multiplier
         multiplier = multiplier + 1
