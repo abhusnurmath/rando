296e625ee3988f38ad3a9970be1f0c28c700ed23
abhusnurmath <bhusnur@gmail.com>
2015-01-23 21:42:50 -0500

    area funcs edited to clarify variable names

--- cit590Examples/Python/functions/areaFuncs.py
+++ cit590Examples/Python/functions/areaFuncs.py
@@ -1,13 +1,34 @@
-import math
-
-def areaOfTriangle(base, height):
-    area = 0.5 * base * height
-    return area
-
-def areaofCircle(radius):
-    area = math.pi * radius ** 2
-    return area
-
-def areaOfSquare(length):
+def area_of_rectangle(length, breadth):
+    '''computes the area of '''
+    # the first value passed to this function
+    # will be stored in length,
+    # the second value passed to this function
+    # will be stored in breadth
     area = length * length
     return area
+
+def area_of_square(s):
+    '''computes the area of a square of side s'''
+    # whatever value is passed to this function
+    # is saved into the variable called s
+    # and then s is used in this function
+    return s ** 2
+
+def main():
+    square_side = 5
+    print ('area of square method 1 '
+    , area_of_square(square_side))
+    
+    #this has no effect since
+    #this variable is local to main
+    length = 723
+
+    print ('area of square method 2 '
+    , area_of_rectangle(square_side, square_side))
+    
+
+if __name__ == "__main__":
+    main()
+    
+
+
