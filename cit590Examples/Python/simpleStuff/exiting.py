d88b3131831d0e7e4441304419ec1139dc079543
abhusnurmath <bhusnur@gmail.com>
2015-01-29 19:50:30 -0500

    how to exit

---
+++ cit590Examples/Python/simpleStuff/exiting.py
@@ -0,0 +1,14 @@
+# program used to demonstrate the proper way to exit loops
+import math
+import sys
+n = 1
+continue_choice = True
+while n < 100 and continue_choice:
+    print 'the sqrt of ', n, 'is ',math.sqrt(n)
+    #print squareroots 10 at a time
+    if n % 10 == 1:
+        continue_choice = raw_input('continue?\n')
+        if continue_choice == 'n' or continue_choice == 'N':
+            return
+    n += 1
+print 'yayyy we printed all the sqrts!!!'
