e12e9a19387ab46ba26431d269ddecfa774348d8
abhusnurmath <bhusnur@gmail.com>
2015-02-12 17:03:28 -0500

    checking in stuff done in class

--- cit590Examples/Python/Files/inClassFileExample.py
+++ cit590Examples/Python/Files/inClassFileExample.py
@@ -5,10 +5,37 @@
 ##When the user enters a file that doesnt exist,
 ##the program exits
 
+def count_lines(list_of_lines):
+    '''count number of lines in a list of lines'''
+    return len(list_of_lines)
+
+def count_words(list_of_lines):
+    '''count words in a list of lines'''
+    word_count = 0
+    for line in list_of_lines:
+        list_of_words = line.split()
+        word_count += len(list_of_words)
+    return word_count
+
+def count_chars(list_of_lines):
+    return 131424
 
 def main():
     #ask for file name
-    print 'i will do this later'
+    filename = raw_input('give me a file name pls \n')
+    #count lines
+    f = open(filename)
+    lines = f.readlines()
+    print count_lines(lines)
+
+    
+    
+    #count words
+    #count characters
+    
+    
+    
+    
 
 if __name__ == '__main__':
     main()
--- cit590Examples/Python/Files/testFileExample.py
+++ cit590Examples/Python/Files/testFileExample.py
@@ -7,4 +7,17 @@ class Test_FileExample(unittest.TestCase):
     def setUp(self):
         self.fileName = 'testfile.txt'
 
+    def test_count_lines(self):
+        num_lines = count_lines(['for', 'cit', '590'])
+        self.assertEqual(num_lines, 3)
+        self.assertEqual(count_lines([]), 0)
+
+    def test_count_words(self):
+         num_words = count_words(['for this', 'cit', '590 something'])
+         self.assertEqual(num_words, 5)
+         self.assertEqual(count_words([]), 0)
+         self.assertEqual(count_words(['a b c', '', 'd']), 4)
+
+    def test_count_chars(self):
+        self.assertEqual(count_chars(['ab b c', '', 'd']), 5)
 unittest.main()
